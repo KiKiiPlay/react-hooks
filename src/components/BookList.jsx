b306a5355f590ea8c69c5b754467385c7b46fe2e
Kiril Vandzsurak <kiril.wandzurak@gmail.com>
2022-11-29 15:56:04 +0100

    Refactoring BookList to function through hooks

--- src/components/BookList.jsx
+++ src/components/BookList.jsx
@@ -1,61 +1,53 @@
-import { Component } from 'react'
-import SingleBook from './SingleBook'
-import { Col, Container, Form, Row } from 'react-bootstrap'
-import CommentArea from './CommentArea'
+import SingleBook from "./SingleBook";
+import { Col, Container, Form, Row } from "react-bootstrap";
+import CommentArea from "./CommentArea";
+import { useEffect, useState } from "react";
 
-class BookList extends Component {
-  state = {
-    searchQuery: '',
-    selectedBook: null,
-  }
+const BookList = ({ books }) => {
+  const [searchQuery, setSearchQuery] = useState("");
+  const [selectedBook, setSelectedBook] = useState(null);
 
-  render() {
-    return (
-      <Container>
-        <Row>
-          <Col md={8}>
-            <Row>
-              <Col>
-                <Form.Group>
-                  <Form.Label>Search</Form.Label>
-                  <Form.Control
-                    type="text"
-                    placeholder="Search here"
-                    value={this.state.searchQuery}
-                    onChange={(e) =>
-                      this.setState({ searchQuery: e.target.value })
-                    }
+  // useEffect(() => {
+  // this code will be executed every time counter changes!
+  // }, [searchQuery]);
+
+  return (
+    <Container>
+      <Row>
+        <Col md={8}>
+          <Row>
+            <Col>
+              <Form.Group>
+                <Form.Label>Search</Form.Label>
+                <Form.Control
+                  type="text"
+                  placeholder="Search here"
+                  value={searchQuery}
+                  onChange={(e) => setSearchQuery(e.target.value)}
+                />
+              </Form.Group>
+            </Col>
+          </Row>
+          <Row>
+            {books
+              .filter((b) => b.title.toLowerCase().includes({ searchQuery }))
+              .map((b) => (
+                <Col xs={3} key={b.asin}>
+                  <SingleBook
+                    book={b}
+                    selectedBook={selectedBook}
+                    changeSelectedBook={(asin) => setSelectedBook(asin)}
                   />
-                </Form.Group>
-              </Col>
-            </Row>
-            <Row>
-              {this.props.books
-                .filter((b) =>
-                  b.title.toLowerCase().includes(this.state.searchQuery)
-                )
-                .map((b) => (
-                  <Col xs={3} key={b.asin}>
-                    <SingleBook
-                      book={b}
-                      selectedBook={this.state.selectedBook}
-                      changeSelectedBook={(asin) =>
-                        this.setState({
-                          selectedBook: asin,
-                        })
-                      }
-                    />
-                  </Col>
-                ))}
-            </Row>
-          </Col>
-          <Col md={4}>
-            <CommentArea asin={this.state.selectedBook} />
-          </Col>
-        </Row>
-      </Container>
-    )
-  }
-}
+                </Col>
+              ))}
+          </Row>
+        </Col>
+        <Col md={4}>
+          <CommentArea asin={selectedBook} />
+        </Col>
+      </Row>
+    </Container>
+  );
+};
 
-export default BookList
+export default BookList;
